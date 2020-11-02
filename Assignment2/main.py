e39c78daddd12f9dd5fe14e3cdbb961660f2aec6
Shichen Lu <shichenl2@gmail.com>
2020-11-01 21:01:42 -0500

    pre-lambdify save

--- Assignment2/main.py
+++ Assignment2/main.py
@@ -1,10 +1,15 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy.io
-from sympy import *
+from sympy import Matrix, symbols, init_printing, cos, sin, sqrt, atan2, latex, diag, eye
+from sympy.utilities.lambdify import lambdify
 init_printing()
 
-def run(printing=True):
+
+def wraptopi(x):
+    return (x + np.pi) % (2 * np.pi) - np.pi
+
+def run(printing=True, EKF=True):
     x_k, y_k, theta_k = symbols("x_k y_k theta_k")
     x_kp, y_kp, theta_kp = symbols("x_{k-1} y_{k-1} \\theta_{k-1}")
     x_l, y_l = symbols("x_l y_l")
@@ -12,7 +17,9 @@ def run(printing=True):
     T = symbols("T")
     d = symbols ("d")
     noise_v, noise_omega = symbols("noise_v, noise_omega")
+    var_v, var_omega = symbols("sigma_v, sigma_omega")
     noise_r, noise_phi = symbols("noise_r, noise_phi")
+    var_r, var_phi = symbols("sigma_r, sigma_phi")
     noises = (noise_v, noise_omega, noise_r, noise_phi)
 
     X_k = Matrix([x_k, y_k, theta_k])
@@ -29,11 +36,15 @@ def run(printing=True):
     F_kp = h.jacobian(X_kp).subs([(noise, 0) for noise in noises])
     Wp_k = h.jacobian(W_k).subs([(noise, 0) for noise in noises])*W_k
     Qp_k = Wp_k * Wp_k.T
+    # Manually do expectation
+    Qp_k = Qp_k.subs([(noise_omega*noise_v, 0), (noise_v**2, var_v), (noise_omega**2, var_omega)])
 
 
     G_k = g.jacobian(X_k).subs([(noise, 0) for noise in noises])
     Np_k = g.jacobian(N_k).subs([(noise, 0) for noise in noises])*N_k
     Rp_k = Np_k * Np_k.T
+    # Manually do expectation
+    Rp_k = Rp_k.subs([(noise_phi*noise_r, 0), (noise_r**2, var_r), (noise_phi**2, var_phi)])
 
     if printing:
         print("F_k-1")
@@ -50,6 +61,8 @@ def run(printing=True):
         print(latex(Rp_k))
 
     ############### EKF ###############
+    if not EKF:
+        return
     '''
     F_kp: 3x3
     P_prior: 3x3
@@ -79,25 +92,51 @@ def run(printing=True):
     omega_var = dataset["om_var"]
     d_ = dataset["d"]
 
-    P_prior = np.diag((1, 1, 0.1))
-    X_prior = np.array([[x_true[0], y_true[0], theta_true[0]]]).T
+    P_prior = diag(1, 1, 0.1)
+    X_prior = Matrix([x_true[0], y_true[0], theta_true[0]])
     Xs = []
 
+    F_kp_l = lambdify([v_k, theta_kp, T], F_kp)
+    Qp_k_l = lambdify([var_v, var_omega, theta_kp, T])
+    Rp_k_l = lambdify([var_r, var_phi])
+    h_l = lambdify([x_kp, y_kp, theta_kp, v_k, omega_k, noise_v, noise_omega, T], h)
+    G_k_l = lambdify([x_k, y_k, theta_k, x_l, y_l, d], G_k)
+    g_l = lambdify([x_k, y_k, theta_k, x_l, y_l, noise_r, noise_phi, d], g)
+
+
+    print("Starting EKF...")
     for i in range(x_true.shape[0]):
-        P_post = F_kp * P_prior * F_kp.T + Qp_k
-        X_post = h.subs([(x_kp, X_prior[0][0]), (y_kp, X_prior[0][1]), (theta_kp, X_prior[0][2]), (v_k, v[i]), (omega_k, omega[i]), (noise_v, 0), (noise_omega, 0)])
+        print(f"Finished {i}/{x_true.shape[0]}")
+        F_kp_ = F_kp.subs([(v_k, v[i][0]), (theta_kp, X_prior[2]), (T, 0.1)])
+        Qp_k_ = Qp_k.subs([(var_v, v_var.item()), (var_omega, omega_var.item()), (theta_kp, X_prior[2]), (T, 0.1)])
+        Rp_k_ = Rp_k.subs([(var_r, r_var.item()), (var_phi, b_var.item())])
+
+        P_post = F_kp_ * P_prior * F_kp_.T + Qp_k_
+        X_post = h.subs([(x_kp, X_prior[0]), (y_kp, X_prior[1]), (theta_kp, X_prior[2]),
+                         (v_k, v[i][0]), (omega_k, omega[i][0]), (noise_v, 0), (noise_omega, 0), (T, 0.1)])
+
         KG = Matrix([[0,0,0],[0,0,0],[0,0,0]])
         X_prior = X_post
-        for j in range(17):
+        for j in range(r.shape[1]):
             if r[i][j] == 0: continue
-            G = G_k.subs([(d, d_)])
-            K = P_post * G.T * (G * P_post * G.T + Rp_k).inv()
+            G = G_k.subs([(x_k, X_post[0]), (y_k, X_post[0]), (theta_k, X_post[2]),
+                          (x_l, landmarks[j][0]), (y_l, landmarks[j][1]), (d, d_.item())])
+            K = P_post * G.T * (G * P_post * G.T + Rp_k_).inv()
+
             KG += K*G
-            X_prior += K * (Matrix([r[i][j], b[i][j]]) - g.subs([(x_k, X_post[0][0]), (y_k, X_post[0][1]), (theta_k, X_post[0][2]), (x_l, landmarks[j][0]), (y_l, landmarks[j][1]), (noise_r, 0), (noise_phi, 0)]))
+            g_ = g.subs([(x_k, X_post[0]), (y_k, X_post[1]), (theta_k, X_post[2]),
+                                      (x_l, landmarks[j][0]), (y_l, landmarks[j][1]), (noise_r, 0),
+                                      (noise_phi, 0), (d, d_.item())])
+            g_[1] = wraptopi(g_[1])
+            X_prior += K * (Matrix([r[i][j], b[i][j]]) - g_)
+            # print(X_prior.evalf())
         P_prior = (eye(3) - KG) * P_post
         Xs.append(X_prior)
 
+    Xs = np.array([Xs]).T
+    err = np.abs(x_true - Xs)
+    print(np.avg(err))
 
 if __name__ == "__main__":
-    run()
+    run(printing=True, EKF=True)
 
